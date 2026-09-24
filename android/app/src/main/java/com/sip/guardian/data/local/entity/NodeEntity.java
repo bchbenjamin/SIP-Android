a10c0511c14f4b08f9ddb0c6e6398f72642561ae
Benjamin Christopher Hermes B <122144062+bchbenjamin@users.noreply.github.com>
2026-09-24 07:36:37 +0530

    fix: mark Room node primary key non-null

--- android/app/src/main/java/com/sip/guardian/data/local/entity/NodeEntity.java
+++ android/app/src/main/java/com/sip/guardian/data/local/entity/NodeEntity.java
@@ -1,11 +1,13 @@
 package com.sip.guardian.data.local.entity;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "nodes")
 public class NodeEntity {
     @PrimaryKey
+    @NonNull
     public String nodeId;
     public String name;
     public String status;
